72e3863ef862500eeebde4abe099ac87084ac80f
neilquizon <55164072+neilquizon@users.noreply.github.com>
2020-03-29 22:58:40 -0700

    Added Model for User and Stock

---
+++ MobileProject/Model/Stock.swift
@@ -0,0 +1,24 @@
+
+import Foundation
+
+struct Stock{
+    var symbol : String
+    var price : Float
+
+    init() {
+        symbol = ""
+        price = 0.0
+    }
+}
+
+
+//symbol": "TSLA",
+//"02. open": "505.0000",
+//"03. high": "525.8000",
+//"04. low": "494.0300",
+//"05. price": "514.3600",
+//"06. volume": "14377408",
+//"07. latest trading day": "2020-03-27",
+//"08. previous close": "528.1600",
+//"09. change": "-13.8000",
+//"10. change percent": "-2.6128%"
---
+++ MobileProject/Model/User.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+
+struct Helper{
+    static var CurrentUser : User?
+}
+
+class User: NSObject {
+    var id : String?
+    var name: String?
+    var credit : Float?
+    var stocks : [String: [Any]]?
+    
+    init(_ dictionary : [String: AnyObject]) {
+        name = dictionary["name"] as? String
+        credit = dictionary["balance"] as? Float
+        stocks = dictionary["stocks"] as? [String:[Any]]
+//        var list = stocks!["stocks"]
+        print("hello")
+    }
+}
